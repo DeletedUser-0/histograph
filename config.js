8c0bded148b5e26d5c5fe99b72a7c6d3e2fd22e2
DeletedUser-0 <deleteduser71@gmail.com>
2026-06-03 09:33:24 +0100

    Stuff

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSy" + "Bw9N-x-D_kFyUCUzuyF8UKvMAmeSR7GPI",
+    authDomain: "infinite-direct-ascendancy.firebaseapp.com",
+    databaseURL: "https://family-tree-tracker-8962c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "infinite-direct-ascendancy",
+    storageBucket: "infinite-direct-ascendancy.appspot.com",
+    messagingSenderId: "847294829102",
+    appId: "1:847294829102:web:your_app_id"
+};
